5b3c9ac015a550f2bd6f73cdfb3c9810a39e26e5
h42m0ny <harmonyleclaire@gmail.com>
2019-12-10 13:38:15 +0100

    remove index.html from lit-beers tuto

--- app/index.js
+++ app/index.js
@@ -13,3 +13,13 @@ var server = app.listen(3000, function (){
 
     console.log('Listening at http://%s:%s',host,port);
 });
+
+app.get('/beers',function(req, res){
+    console.log('Received request for beers from', req.ip);
+    res.send('Hello beers !');
+});
+
+app.get('/beers/:beerId', function(req, res){
+    console.log('Recieived request from'+req.params['beerId']+' from', req.ip);
+    res.send('Hello beer '+req.parms['beerId']);
+});
