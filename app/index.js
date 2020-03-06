d859ae7555689167f7f6449803bc3f63b6fc5b24
h42m0ny <harmonyleclaire@gmail.com>
2020-03-06 14:49:58 +0100

    update connection uri database variabilised

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 var MongoClient = require('mongodb').MongoClient;
-var url = `mogodb://${process.env.MONGODB_ADDON_USER}:${process.env.MONGODB_ADDON_USER}@${process.env.MONGODB_ADDON_URI}:${process.env.MONGODB_ADDON_PORT}`;
+var url = `${process.env.MONGODB_ADDON_URI}`;
 
 MongoClient.connect(url,function(err,client){
     console.log("Connected correctly to MongoDB server.");
